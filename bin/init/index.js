988ee43aa505b647ac6d53773d15024e1d9cc8ea
briskcasual <briskcasual@gmail.com>
2020-03-19 19:14:47 -0400

    can set folder name

--- bin/init/index.js
+++ bin/init/index.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 
 let copyDir = require('../../lib/copydir/index.js').copyDir,
+path = require('path'),
 log = require('../../lib/log/index.js');
 
-copyDir('./site-start', './site-demo')
-.then(()=>{
 
+let source = path.join(__dirname, 'site-start'),
+target = path.resolve(process.argv[2] || path.join(process.cwd(), 'site-demo'));
+copyDir(source, target)
+.then(()=>{
+    log('done', 'success');
 });
