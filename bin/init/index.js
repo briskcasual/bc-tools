2eb3287042ae82b1288ad3515b9ded774ff8e4d8
briskcasual <briskcasual@gmail.com>
2020-03-19 08:34:33 -0400

    bc-init is copying the start folder

--- bin/init/index.js
+++ bin/init/index.js
@@ -1,1 +1,9 @@
 #!/usr/bin/env node
+
+let copyDir = require('../../lib/copydir/index.js').copyDir,
+log = require('../../lib/log/index.js');
+
+copyDir('./site-start', './site-demo')
+.then(()=>{
+
+});
